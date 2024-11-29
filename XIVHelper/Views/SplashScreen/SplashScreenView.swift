5aeaa03d2c13dc6f074cbc9979d4f7c2a58fdc4c
dzhared <25894006+dzhared@users.noreply.github.com>
2024-11-28 23:52:41 -0600

    Refactor splash screen
    Refactors splash screen to allow ContentView to begin loading while the splash screen is showing.

--- XIVHelper/Views/SplashScreen/SplashScreenView.swift
+++ XIVHelper/Views/SplashScreen/SplashScreenView.swift
@@ -6,44 +6,72 @@ struct SplashScreenView: View {
     // MARK: Properties
 
     /// Whether the app is active, following the splash screen's display.
-    @State private var isActive: Bool = false
+    @State private var backgroundOpacity: Double = 1
 
-    /// The starting size of the splash screen.
-    @State private var size: Double = 0.8
+    /// The current color scheme, used to determine what color to show for the background.
+    @Environment(\.colorScheme) var colorScheme
 
     /// The starting opacity of the splash screen.
-    @State private var opacity: Double = 0
+    @State private var logoOpacity: Double = 0
+
+    /// Whether the splash screen exists. Used to deinitialize after finished.
+    @State private var showSplashScreen: Bool = true
 
     // MARK: Body
 
     var body: some View {
-        if isActive {
+        ZStack {
             ContentView()
-        } else {
-            VStack {
-                VStack(spacing: 16) {
-                    Image(.splashScreenIcon)
-                    Text(AppStrings.General.appName)
-                        .font(.system(size: 48))
-                        .foregroundStyle(.primary)
-                }
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeInOut(duration: 0.8)) {
-                        size = 1.0
-                        opacity = 1.0
-                    }
-                }
-            }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                    withAnimation(.easeIn(duration: 0.8)) {
-                        isActive = true
-                    }
-                }
+            if showSplashScreen {
+                splashScreen()
             }
         }
+    }
+
+    // MARK: View Builder
+
+    /// The splash screen.
+    @ViewBuilder private func splashScreen() -> some View {
+        // Color background
+        Color.init(colorScheme == .dark ? .black : .white)
+            .opacity(backgroundOpacity)
+            .ignoresSafeArea()
+
+        // Logo
+        VStack(spacing: 16) {
+            Image(.splashScreenIcon)
+            Text(AppStrings.General.appName)
+                .font(.system(size: 48))
+                .foregroundStyle(.primary)
+        }
+        .opacity(logoOpacity)
+        .onAppear {
+            Task { @MainActor in
+                await animateSplashScreen()
+            }
+        }
+    }
+
+    // MARK: Private Methods
+
+    /// The loop responsible for the entire splash screen animation.
+    @MainActor private func animateSplashScreen() async {
+        // Fade in the logo
+        try? await Task.sleep(seconds: 0.5)
+        withAnimation(.easeInOut(duration: 0.5)) {
+            logoOpacity = 1.0
+        }
+
+        // Fade the splash screen out
+        try? await Task.sleep(seconds: 2)
+        withAnimation(.linear(duration: 0.5)) {
+            backgroundOpacity = 0
+            logoOpacity = 0
+        }
+
+        // Deinitialize the splash screen after completed
+        try? await Task.sleep(seconds: 2.5)
+        showSplashScreen = false
     }
 }
 
